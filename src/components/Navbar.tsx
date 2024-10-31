05512933438ccc7bb6c2c602dd934328ff2b5a10
Don <efpabua1@up.edu.ph>
2024-10-31 21:40:37 +0800

    Added changes to the navbar and the homepage layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import logo from './logo.svg'
+import menu from './menu.svg'
+import person from './person.svg'
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -22,20 +25,10 @@ export default function Navbar() {
               <span className="sr-only">Your Company</span>
               <img
                 alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                src={logo}
                 className="h-8 w-auto"
               />
             </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-            </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
@@ -46,7 +39,12 @@ export default function Navbar() {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
+            <button><img alt="" src={person} className="h-8 w-auto"/></button>
+            </a>
+          </div>
+          <div className="lg:flex lg:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <button><img alt="" src={menu} className="h-8 w-auto"/></button>
             </a>
           </div>
         </nav>
@@ -58,7 +56,7 @@ export default function Navbar() {
                 <span className="sr-only">Your Company</span>
                 <img
                   alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                  src={logo}
                   className="h-8 w-auto"
                 />
               </a>
