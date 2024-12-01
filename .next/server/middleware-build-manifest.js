cc1419c37705e5bf171ef4379897877cb08a9609
Shaina <sdtalisay@up.edu.ph>
2024-12-01 18:46:08 +0800

    work na pls

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -10,11 +10,6 @@ self.__BUILD_MANIFEST = {
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -30,10 +25,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/events.js"
     ],
-    "/login": [
+    "/memberspageofficerview": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/login.js"
+      "static/chunks/pages/memberspageofficerview.js"
     ],
     "/orgpage": [
       "static/chunks/webpack.js",
